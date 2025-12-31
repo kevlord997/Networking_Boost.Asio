45ec8eb802a5c1a2d13d627944f6486eeb830b93
kevlord997 <dankasa997@gmail.com>
2025-12-31 23:51:14 +0100

    Project2 refactor

--- Project2_async_echo/src/server.cpp
+++ Project2_async_echo/src/server.cpp
@@ -2,28 +2,88 @@
 #include <iostream>
 #include <memory>
 #include <array>
+#include <atomic>
 
 using boost::asio::ip::tcp;
 using namespace boost::asio;
 
-class session
-      : public std::enable_shared_from_this<session>
+class session;
+
+
+// Server class
+class server
 {
   public:
-    session(any_io_executor exec)
-          : socket_(exec),
-            strand_(make_strand(exec))
+    server(io_context &io_ctx, short port)
+          : acceptor_(io_ctx, tcp::endpoint(tcp::v4(), port))
+    {
+        start_accept();
+    }
+
+    void on_connect()
     {
         ++active_connections;
         std::cout << "Active connections: "
                   << active_connections.load() << "\n";
     }
 
-    ~session()
+    void on_disconnect()
     {
         --active_connections;
         std::cout << "Active connections: "
                   << active_connections.load() << "\n";
+    }
+
+
+  private:
+    void start_accept();
+    void handle_accept(const std::shared_ptr<session> &new_session, const boost::system::error_code &ec);
+//    void start_accept()
+//    {
+//        auto new_session = std::make_shared<session>(acceptor_.get_executor(), *this);
+//
+//        acceptor_.async_accept(
+//              new_session->socket(),
+//              [this, new_session](const boost::system::error_code &ec) {
+//                  handle_accept(new_session, ec);
+//              });
+//    }
+
+//    void handle_accept(const std::shared_ptr<session> &new_session,
+//                       const boost::system::error_code &ec)
+//    {
+//        if (!ec) {
+//            auto endpoint = new_session->socket().remote_endpoint();
+//            std::cout << "New client connected: "
+//                      << endpoint.address().to_string() << ":"
+//                      << endpoint.port() << "\n";
+//            new_session->start();
+////                std::cout << "New client connected\n";
+//        } else {
+//            std::cerr << "Accept error: " << ec.message() << "\n";
+//        }
+//        start_accept();  // Accept next
+//    }
+
+    tcp::acceptor acceptor_;
+    std::atomic<int> active_connections{0};
+};
+
+class session
+      : public std::enable_shared_from_this<session>
+{
+  public:
+    session(any_io_executor exec, server& srv)
+          : socket_(exec),
+            server_(srv),
+            strand_(make_strand(exec))
+    {
+        server_.on_connect();
+    }
+
+    ~session()
+    {
+        server_.on_disconnect();
     }
 
     tcp::socket &socket() { return socket_; }
@@ -75,53 +135,37 @@ class session
     }
 
     tcp::socket socket_;
+    server &server_;
     std::vector<char> buffer_;
-    std::atomic<int> active_connections{0};
     strand<any_io_executor> strand_;
 };
 
-// Server class
-class server
+void server::start_accept()
 {
-  public:
-    server(io_context &io_ctx, short port)
-          : acceptor_(io_ctx, tcp::endpoint(tcp::v4(), port))
-    {
-        start_accept();
-    }
+    auto new_session = std::make_shared<session>(acceptor_.get_executor(), *this);
 
+    acceptor_.async_accept(
+          new_session->socket(),
+          [this, new_session](const boost::system::error_code &ec) {
+              handle_accept(new_session, ec);
+          });
+}
 
-  private:
-    tcp::acceptor acceptor_;
-
-    void start_accept()
-    {
-        auto new_session = std::make_shared<session>(acceptor_.get_executor());
-
-        acceptor_.async_accept(
-              new_session->socket(),
-              [this, new_session](const boost::system::error_code &ec) {
-                  handle_accept(new_session, ec);
-              });
-    }
-
-    void handle_accept(std::shared_ptr<session> new_session,
-                       const boost::system::error_code &ec)
-    {
-        if (!ec) {
-            auto endpoint = new_session->socket().remote_endpoint();
-            std::cout << "New client connected: "
-                      << endpoint.address().to_string() << ":"
-                      << endpoint.port() << "\n";
-            new_session->start();
+void server::handle_accept(const std::shared_ptr<session> &new_session,
+                   const boost::system::error_code &ec)
+{
+    if (!ec) {
+        auto endpoint = new_session->socket().remote_endpoint();
+        std::cout << "New client connected: "
+                  << endpoint.address().to_string() << ":"
+                  << endpoint.port() << "\n";
+        new_session->start();
 //                std::cout << "New client connected\n";
-        } else {
-            std::cerr << "Accept error: " << ec.message() << "\n";
-        }
-        start_accept();  // Accept next
+    } else {
+        std::cerr << "Accept error: " << ec.message() << "\n";
     }
-};
-
+    start_accept();  // Accept next
+}
 
 int main()
 {
